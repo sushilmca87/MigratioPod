5ace71129e75a0164c091a9d2afb8787979c6b92
Sushil Kumar Singh <SushilKumar.Singh@mavenir.com>
2022-03-01 07:59:19 +0530

    Fixed

---
+++ Employee.swift
@@ -0,0 +1,16 @@
+//
+//  Employee.swift
+//  MigrationSDK
+//
+//  Created by Sushil Kumar Singh on 01/03/22.
+//
+
+import Foundation
+open class Employee {
+   public init(){
+        print("show method")
+    }
+    public func showMethod(){
+        print("show call")
+    }
+}
